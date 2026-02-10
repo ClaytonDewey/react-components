8e718ef92e264c8e27a592c2ea6d468a041fa3e0
ClaytonDewey <clay@dryadmedia.com>
2026-02-10 16:05:18 -0600

    refactor: Change to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import Button from './components/Button';
+
 const App = () => {
   return (
     <main>
       <h1>hello there</h1>
+      <Button />
     </main>
   );
 };
 export default App;
+// https://react-aria.adobe.com/Button
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,9 @@ import './sass/style.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
 root.render(
   <React.StrictMode>
     <App />
